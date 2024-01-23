1478ab0d6aecf15d50e3034955d478d93f05881d
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-01-23 17:02:40 +0530

    commitzen

--- snigdhaos_functions.py
+++ snigdhaos_functions.py
@@ -12,3 +12,19 @@ from os import path, getlogin
 get_distro = id()
 superuser = getlogin()
 home = "/home/" + str(superuser)
+gpg_config = "/etc/pacman.d/gnupg/gpg.conf"
+gpg_config_local = home + "/.gnupg/gpg.conf"
+
+gpg_config_original = "/usr/share/snigdhaos-downloader/data/any/gpg.conf"
+gpg_config_local_original = "/usr/share/snigdhaos-downloader/data/any/gpg.conf"
+
+def get_lines(files):
+    try:
+        if path.isfile(files):
+            with open(files, "r", encoding="utf-8") as f:
+                lines = f.readlines()
+                f.close()
+            return lines
+    except Exception as error:
+        print(error)
+
