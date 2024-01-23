285ef9f64a04600599bf6c32719f6d9ed0da079d
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-01-23 16:40:37 +0530

    add asic files

---
+++ snigdhaos-downloader.py
@@ -0,0 +1,6 @@
+#####################################################################
+# Author    : Eshan Roy <eshan@snigdhaos.org>                       #
+# Web       : eshan.snigdhaos.org                                   #
+# Lead Maintainer & Developer @ Snigdha OS!                         #
+#####################################################################
+
