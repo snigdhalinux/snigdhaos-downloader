9b1eaddb51e32556e3bec3721c5919b54d03eca9
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-01-28 02:37:38 +0530

    add base files

---
+++ snigdhaos-installer.py
@@ -0,0 +1,13 @@
+##################################################################################
+# Author: Eshan Roy <eshan@snigdhaos.org>
+# URL : https://eshan.snigdhaos.org
+# Lead Maintainer & Developer @ Snigdha OS
+##################################################################################
+
+import gi
+gi.require_version("Gtk", "3.0")
+import os
+# Need to make a function first for everything we do in backend
+# import function as fn _>
+
+# base_dir = fn.path
