db469a64a20d16e110178ea6bdc133190ef45d93
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-01-28 06:33:46 +0530

    tired

---
+++ fixes_gui.py
@@ -0,0 +1,12 @@
+##################################################################################
+# Author: Eshan Roy <eshan@snigdhaos.org>
+# URL : https://eshan.snigdhaos.org
+# Lead Maintainer & Developer @ Snigdha OS
+##################################################################################
+
+def gui(self, Gtk, vboxstack, fn, fixes):
+    # I am not going to write gui 
+    # Iconized will write instead
+    # icon@snigdhaos.org
+    # i'm just completing the backend 
+    
