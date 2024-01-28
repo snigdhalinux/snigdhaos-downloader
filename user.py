e4f42ed07aa5a224706e98f5747f892c53784f6f
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-01-28 05:50:56 +0530

    update user.py

--- user.py
+++ user.py
@@ -14,3 +14,24 @@ def create_user(self):
     atype = self.combo_account_type.get_active_text()
     password = self.hbox_password.get_text()
     confirm_password = self.hbox_confirm_password.get_text()
+
+    if (len(username) > 0 and len(name) > 0 and len(password) > 0 and len(confirm_password) > 0):
+        if password == confirm_password:
+            user_password = "echo " + username + ":" + password
+            try:
+                command = "groupadd -r sambashare"
+                fn.subprocess.call(command.split(" "),shell=False,stdout=fn.PIPE, stderr=fn.STDOUT,)
+            except Exception as e:
+                print(e)
+            if password == confirm_password:
+                if atype == "Administrator":
+                    useradd = ('useradd -m -G audio,video,network,storage,rfkill,wheel,sambashare -c "' + name + '" -s /bin/bash ' + username)
+                    fn.system(useradd)
+                    fn.system(user_password + " | " + "chpasswd -c SHA512")
+                else:
+                    useradd = ('useradd -m -G audio,video,network,storage,rfkill,sambashare -c "' + name + '" -s /bin/bash ' + username)
+                    fn.system(useradd)
+                    fn.system(user_password + " | " + "chpasswd -c SHA512")
+                print("User Created Successfully :)")
+                GLib.idle_add(fn.show_in_app_notfication, self, "User Created Successfully :)")
+                
