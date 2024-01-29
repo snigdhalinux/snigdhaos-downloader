58b746e6674c0a6b330c027c923cc537f234cfaa
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-01-30 03:49:53 +0530

    69

---
+++ functions.py
@@ -0,0 +1,6 @@
+##########################################################################
+# Author : Eshan Roy <eshan@snigdhaos.org>
+# URI: https://eshan.snigdhaos.org
+# Lead Maintainer @ Snidgha OS
+##########################################################################
+
