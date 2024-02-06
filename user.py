705bc0c4523a8c8223ea8f63ba7131955fea906b
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-02-06 15:31:54 +0530

    update

---
+++ user.py
@@ -0,0 +1,11 @@
+# Author : Eshan Roy <esha@snigdhaos.org>
+
+import functions as fn
+from functions import GLib
+
+def create_user(self):
+    username = 
+    name = 
+    actype = 
+    password = 
+    condifrm_password = 
---
+++ user_gui.py
@@ -0,0 +1,4 @@
+# Use pyqt5
+
+def gui(self, Gtk, vboxStack10, user, fn):
+    hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
