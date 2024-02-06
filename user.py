aa7931dae548182e37217062c6359fe7961f85c0
Abhiraj Roy (iconized) <157954129+iconized@users.noreply.github.com>
2024-02-06 10:18:59 +0000

    Update user.py

--- user.py
+++ user.py
@@ -6,6 +6,6 @@ from functions import GLib
 def create_user(self):
     username = self.hbox_username.get_text()
     name = self.hbox_name.get_text()
-    actype = self.combo_account_type.get_text()
+    actype = self.combo_account_type.get_active_text()
     password = self.hbox_password().get_text()
     confirm_password = self.hbox_confirm_password().get_text()
