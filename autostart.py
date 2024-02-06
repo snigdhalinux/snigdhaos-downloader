02c8c5044a59c9cd90daa1ea96ceb75e0aa1fc1e
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-02-06 14:52:49 +0530

    init autostart.py

--- autostart.py
+++ autostart.py
@@ -24,4 +24,35 @@ def get_startup(name):
             return True
     else:
         return state
-    
+
+
+def add_autostart(self, name, content, comment, toexec):
+    lists = list(fn.listdir(fn.home + "/.config/autostart"))
+    if not name + ".desktop" in lists:
+        content = (
+            "[Desktop Entry]\n\
+                Encoding=UTF-8\n\
+                    Version=1.0\n\
+                        Type=Application\n\
+                            Name="
+                            + name
+                            + "\n\
+                                Comment="
+                                + comment
+                                + "\n\
+                                    Exec="
+                                    + toexec
+                                    + "\n\
+                                        TryExec="
+                                        + toexec
+                                        + "\n\
+                                            StartupNotify=false\n\
+                                                X-Gnome-Autostart-enabled=true\n\
+                                                    Terminal=false\n\
+                                                        Hidden=false\n"
+        )
+        with open(fn.home + "/.config/autostart/" + name + ".desktop", "w", encoding="utf-8") as f:
+            f.write(content)
+            f.close()
+        self.add_row(name)
+        
